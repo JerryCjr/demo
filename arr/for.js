da942651c2e58848908c3ea624c4f5d1ebd6c59a
程晋瑞 <chengjinrui@babyfs.cn>
2020-09-03 07:56:40 +0800

    array unique

---
+++ arr/for.js
@@ -0,0 +1,74 @@
+const {
+  array1,
+  array2,
+  array3,
+  array4,
+  array5,
+  array6,
+} = require("./testData.js");
+
+/**
+ * @function 数组且非空
+ * @param {Array} _array
+ * @returns Boolean
+ */
+function notEmpty(_array) {
+  if (_array && Array.isArray(_array) && _array.length) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/**
+ * @function 双层for循环+splice
+ * @description TODO: 时间和空间复杂度是多少
+ * @param {Array} _array
+ */
+function for_01(_array) {
+  if (notEmpty(_array)) {
+    for (let i = 0; i < _array.length; i++) {
+      for (let j = i + 1; j < _array.length; j++) {
+        if (_array[i] === _array[j]) {
+          _array.splice(j, 1);
+          j--;
+        }
+      }
+    }
+    return _array;
+  }
+}
+
+// console.log(for_01(array1));
+// console.log(for_01(array2));
+// console.log(for_01(array3));
+// console.log(for_01(array4));
+// console.log(for_01(array5));
+console.log(for_01(array6));
+// ! NAN {} 无效
+
+/**
+ * @function 一层for循环+indexOf
+ * @description TODO: 时间和空间复杂度是多少
+ * @param {Array} _array
+ */
+function for_02(_array) {
+  if (notEmpty(_array)) {
+    let r = [];
+    for (let i = 0; i < _array.length; i++) {
+      if (_array.indexOf(_array[i]) === i) {
+        r.push(_array[i]);
+      }
+    }
+    return r;
+  }
+}
+
+// console.log(for_02(array1));
+// console.log(for_02(array2));
+// console.log(for_02(array3));
+// console.log(for_02(array4));
+// console.log(for_02(array5));
+console.log(for_02(array6));
+// ! {} 无效
+// ! NAN 无法识别 array6 有两个NAN 但是返回的数组两个都没了
