46abeac8f52e2c3ec932e6371f4a0dcf2127e53b
Jerry <jerryc_js@163.com>
2020-09-08 15:14:38 +0800

    使用yarn构建,格式化代码,新增企业微信测试目录

--- jsbridge/src/views/Bridge.vue
+++ jsbridge/src/views/Bridge.vue
@@ -2,41 +2,46 @@
   <div class="root">
     <h1>bridge</h1>
     <div class="app" v-if="isApp">
-      <p class="share-app-message" @click="nativeShareMenu">唤起原生的分享菜单</p>
-      <p class="share-app-message" @click="shareAppMessage">唤起原生的分享好友</p>
+      <p class="share-app-message" @click="nativeShareMenu">
+        唤起原生的分享菜单
+      </p>
+      <p class="share-app-message" @click="shareAppMessage">
+        唤起原生的分享好友
+      </p>
       <p class="share-timeline" @click="shareTimeline">唤起原生的分享朋友圈</p>
-      <p class="share-timeline" @click="nativeShareMenuToShareImage">唤起原生的分享菜单(分享图片)</p>
+      <p class="share-timeline" @click="nativeShareMenuToShareImage">
+        唤起原生的分享菜单(分享图片)
+      </p>
     </div>
   </div>
 </template>
 
 <script>
-import bridge from "babyfs-jsbridge";
-import env from "babyfs-env";
-import { ref, defineComponent } from "vue";
+import bridge from 'babyfs-jsbridge';
+import env from 'babyfs-env';
+import { ref, defineComponent } from 'vue';
 export default defineComponent({
-  name: "Bridge",
+  name: 'Bridge',
   setup() {
-    const isApp = ref(env.app === env.EnumApp["babyfs"]);
-
+    const isApp = ref(env.app === env.EnumApp['babyfs']);
     return {
-      isApp
+      isApp,
     };
   },
   methods: {
     shareHandler(params) {
       if (!params || params === {}) {
-        alert("参数不能为空");
+        alert('参数不能为空');
         return;
       }
-      bridge.callHandler("doShare", JSON.stringify(params));
+      bridge.callHandler('doShare', JSON.stringify(params));
     },
     shareAppMessage() {
       const params = {
         channel: 1,
         url: window.location.href,
-        title: "微信好友",
-        description: "分享给微信好友"
+        title: '微信好友',
+        description: '分享给微信好友',
       };
 
       this.shareHandler(params);
@@ -45,8 +50,8 @@ export default defineComponent({
       const params = {
         channel: 2,
         url: window.location.href,
-        title: "朋友圈",
-        description: "分享朋友圈"
+        title: '朋友圈',
+        description: '分享朋友圈',
       };
 
       this.shareHandler(params);
@@ -55,19 +60,19 @@ export default defineComponent({
       const params = {
         channel: 0,
         url: window.location.href,
-        title: "朋友圈",
-        description: "分享朋友圈"
+        title: '朋友圈',
+        description: '分享朋友圈',
       };
       this.shareHandler(params);
     },
     nativeShareMenuToShareImage() {
       const params = {
         channel: 0,
-        posterUrl: "https://i.s.babyfs.cn/1bafac0fe27a416cba04c476324bce22.jpg"
+        posterUrl: 'https://i.s.babyfs.cn/1bafac0fe27a416cba04c476324bce22.jpg',
       };
       this.shareHandler(params);
-    }
-  }
+    },
+  },
 });
 </script>
 
---
+++ jsbridge/src/views/Qyweixin.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="root"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'QyweixinTest',
+});
+</script>
+
+<style lang="scss" scoped></style>
